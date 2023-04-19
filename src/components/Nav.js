89a8bc9e8758bd64146f1fced9199397cbe4af80
Kidd Sng <sngkidd@outlook.com>
2023-04-19 22:14:52 +0800

    Update files
    Create components and update logo images

---
+++ src/components/Nav.js
@@ -0,0 +1,19 @@
+import logo from "../logo.svg";
+
+const Nav = () => {
+    return (
+        <>
+            <img src={logo} alt="Logo" />
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">About</a></li>
+                <li><a href="#">Menu</a></li>
+                <li><a href="#">Reservations</a></li>
+                <li><a href="#">Order Online</a></li>
+                <li><a href="#">Login</a></li>
+            </ul>
+        </>
+    );
+};
+
+export default Nav;
